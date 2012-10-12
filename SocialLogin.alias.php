ef3522dede25123575a7acaac266fd1a1fddd6b7
luft-on <lufton@gmail.com>
2012-10-12 20:08:54 +0300

    First initial commit of SocialLogin v0.9.5
    Change-Id: Ib24e3225e7d1189305b88f0e8ae06f36d1b5d82d

---
+++ SocialLogin.alias.php
@@ -0,0 +1,11 @@
+<?
+$aliases = array();
+/** English */
+$aliases['en'] = array(
+    'SocialLogin' => array('SocialLogin'),
+);
+ 
+/** Russian */
+$aliases['ru'] = array(
+        'SocialLogin' => array('Социальная_авторизация'),
+);
---
+++ SocialLogin.php
@@ -0,0 +1,20 @@
+<?
+if (!defined('MEDIAWIKI')) die('Not an entry point.');
+$wgExtensionCredits['specialpage'][] = array(
+        'name' => 'SocialLogin',
+        'author' => 'Luft-on',
+        'url' => 'http://www.mediawiki.org/wiki/Extension:SocialLogin',
+        'descriptionmsg' => 'sl-desc',
+        'version' => '0.9.5',
+);
+ 
+$dir = dirname(__FILE__) . '/';
+ 
+$wgAutoloadClasses['SocialLogin'] = $dir . 'SocialLogin.body.php'; # Попросите MediaWiki загрузить тело основного файла.
+foreach ($wgSocialLoginServices as $key => $value) {
+	$name = str_replace('.', '_', $key);
+	$wgAutoloadClasses[$name] = $dir . "/plugins/$key.php";
+}
+$wgExtensionMessagesFiles['sociallogin'] = $dir . 'SocialLogin.i18n.php';
+$wgExtensionAliasesFiles['sociallogin'] = $dir . 'SocialLogin.alias.php';
+$wgSpecialPages['sociallogin'] = 'SocialLogin'; # Сообщите MediaWiki о Вашей новой спецстранице.
