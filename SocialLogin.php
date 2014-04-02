d3ff1d9e61585e463f6c3544f75dc976be55cf0c
Siebrand Mazeland <siebrand@kitano.nl>
2014-04-02 19:10:01 +0200

    Migrate to JSON i18n
    Procedure per https://www.mediawiki.org/wiki/Manual:GenerateJsonI18n.php
    with shim.
    
    Change-Id: I6d04ca9d0f90c25e3de90aa415e0a85fc5d45c87

--- SocialLogin.php
+++ SocialLogin.php
@@ -1,20 +1,23 @@
 <?php
 if (!defined('MEDIAWIKI')) die('Not an entry point.');
+
 $wgExtensionCredits['specialpage'][] = array(
         'name' => 'SocialLogin',
         'author' => 'Luft-on',
         'url' => 'http://www.mediawiki.org/wiki/Extension:SocialLogin',
         'descriptionmsg' => 'sl-desc',
-        'version' => '0.9.5',
+        'version' => '0.10.0',
 );
- 
+
 $dir = dirname(__FILE__) . '/';
- 
+
 $wgAutoloadClasses['SocialLogin'] = $dir . 'SocialLogin.body.php'; # Попросите MediaWiki загрузить тело основного файла.
 foreach ($wgSocialLoginServices as $key => $value) {
 	$name = str_replace('.', '_', $key);
 	$wgAutoloadClasses[$name] = $dir . "/plugins/$key.php";
 }
-$wgExtensionMessagesFiles['sociallogin'] = $dir . 'SocialLogin.i18n.php';
-$wgExtensionAliasesFiles['sociallogin'] = $dir . 'SocialLogin.alias.php';
+
+$wgMessagesDirs['SocialLogin'] = __DIR__ . '/i18n';
+$wgExtensionMessagesFiles['SocialLogin'] = $dir . 'SocialLogin.i18n.php';
+$wgExtensionAliasesFiles['SocialLoginAlias'] = $dir . 'SocialLogin.alias.php';
 $wgSpecialPages['sociallogin'] = 'SocialLogin'; # Сообщите MediaWiki о Вашей новой спецстранице.
