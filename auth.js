ed77535c6b99eb16985ff145440fb5cb0957920b
Umherirrender <umherirrender_de.wp@web.de>
2020-09-05 23:13:25 +0200

    build: Replace jshint/jsonlint with eslint
    Fix issues with eslint --fix
    
    Bug: T210365
    Bug: T220036
    Change-Id: I3cb6b306eb91711993b24710e97ef07efe961b5c

--- auth.js
+++ auth.js
@@ -1,59 +1,54 @@
-/*jshint unused:false */
-function openWindow(url, title, width, height) {
-	var left = (screen.width - width) / 2;
-	var top = (screen.height - height) / 2;
-	return window.open(url, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width='+width+', height='+height+', top='+top+', left='+left);
+function openWindow( url, title, width, height ) {
+	var left = ( screen.width - width ) / 2,
+		top = ( screen.height - height ) / 2;
+	return window.open( url, title, 'toolbar=no, location=no, directories=no, status=no, menubar=no, scrollbars=no, resizable=no, copyhistory=no, width=' + width + ', height=' + height + ', top=' + top + ', left=' + left );
 }
 
-function unlink(profile) {
-	jQuery.ajax({
+function unlink( profile ) {
+	jQuery.ajax( {
 		url: '/Special:SocialLogin',
-		data: {action: 'unlink', profile: profile},
-		success: function(response) {
-			if (response === 'yes') {
-				jQuery('#' + profile.replace('@', '_').replace('.', '_')).remove();
+		data: { action: 'unlink', profile: profile },
+		success: function ( response ) {
+			if ( response === 'yes' ) {
+				jQuery( '#' + profile.replace( '@', '_' ).replace( '.', '_' ) ).remove();
 			} else {
-				window.alert('Не удалось отсоединить профиль социальной сети.');
+				window.alert( 'Не удалось отсоединить профиль социальной сети.' );
 			}
 		}
-	});
+	} );
 }
 
-function gup(url, name) {
-	name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
-	var regexS = "[\\#&]"+name+"=([^&#]*)";
-	var regex = new RegExp( regexS );
-	var results = regex.exec( url );
-	return (results === null)?"":results[1];
+function gup( url, name ) {
+	name = name.replace( /[\[]/, '\\\[' ).replace( /[\]]/, '\\\]' );
+	var regexS = '[\\#&]' + name + '=([^&#]*)',
+		regex = new RegExp( regexS ),
+		results = regex.exec( url );
+	return ( results === null ) ? '' : results[ 1 ];
 }
 
-function tryLogin(data, cb) {
+function tryLogin( data, cb ) {
 	var formText = '<form action="" method="post"><input type="hidden" name="action" value="login" />';
-	jQuery.each(data, function(key, value) {
+	jQuery.each( data, function ( key, value ) {
 		formText += '<input type="hidden" name="' + key + '" value="' + value + '" />';
-	});
+	} );
 	formText += '</form>';
-	var form = jQuery(formText);
-	jQuery('body').append(form);
-	jQuery(form).submit();
+	var form = jQuery( formText );
+	jQuery( 'body' ).append( form );
+	jQuery( form ).trigger( 'submit' );
 }
 
-function hacking() {
-	window.alert('Что-то не так! Возможно вы не тот, за кого себя выдаёте, ай-ай-ай!');
-}
-
-function login(url, cb) {
-	var win = openWindow(url, "sl", 620, 370);
-		var pollTimer = window.setInterval(function() { 
+function login( url, cb ) {
+	var win = openWindow( url, 'sl', 620, 370 ),
+		pollTimer = window.setInterval( function () {
 			try {
-				if (win.document.URL.indexOf(window.document.location.host) >= 0) {
-					window.clearInterval(pollTimer);
-					var url = win.document.URL;
-					var code = gup(url, 'code');
+				if ( win.document.URL.indexOf( window.document.location.host ) >= 0 ) {
+					window.clearInterval( pollTimer );
+					var url = win.document.URL,
+						code = gup( url, 'code' );
 					win.close();
-					cb(code);
+					cb( code );
 				}
-			} catch(e) {
+			} catch ( e ) {
 			}
-		}, 50);
+		}, 50 );
 }
